21babbbbe623085484c31402091a43b54717c1ae
Dilmini1027 <167851870+Dilmini1027@users.noreply.github.com>
2025-05-05 16:27:58 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import HomePage from './HomePage';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
