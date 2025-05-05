8b621002846ed0c34fd57152b9d24e6032423d7a
Dilmini1027 <167851870+Dilmini1027@users.noreply.github.com>
2025-05-05 19:26:27 +0530

    delete file

--- App.js
+++ App.js
@@ -1,13 +1,1 @@
-import HomePage from './HomePage';
-import "@fortawesome/fontawesome-free/css/all.min.css";
 
-
-function App() {
-  return (
-    <div className="App">
-      <HomePage />
-    </div>
-  );
-}
-
-export default App;
